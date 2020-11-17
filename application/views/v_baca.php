1f1620e72e47574eb03d2d7f095d4f5e5635ab3d
ddn05 <dadannazril@gmail.com>
2020-11-17 22:11:24 +0700

    databasae

--- application/views/v_baca.php
+++ application/views/v_baca.php
@@ -2,7 +2,7 @@
 
 <section id="featured" class="featured">
         <div class="container">
-            <img src="<?php echo base_url();?>uploads/img/<?php echo $baca->cover?>" class="img-fluid" alt="Responsive image">
+            <img src="<?php echo base_url();?>uploads/img/<?php echo $baca->cover?>" class="img-fluid" alt="Responsive image"><br>
             <span class="badge badge-info mt-3"><?php echo date('d/m/Y',strtotime($baca->tgl)); ?></span>
             <h3 class="mt-2"><strong><?php echo $baca->judul ?></strong></h3>
             <hr>
